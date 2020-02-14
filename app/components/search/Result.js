def4b3eb1047d70daf0cbd6d515fade5fccb8bfc
Stela <stella.plamenova@gmail.com>
2020-02-14 11:04:22 +0100

    tidied up the result view

---
+++ app/components/search/Result.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import TagItem from '../TagItem';
+import UserRow from '../user/UserRow';
+import FilmCard from '../../components/FilmCard';
+
+const Result = props => {
+
+    switch (props.type) {
+        case 'users':
+            return <UserRow user={props.result} size='large' />
+        case 'tags':
+            return <TagItem tagName={props.result} style={{marginRight: 5, marginVertical: 5}} />
+        default:
+            return <FilmCard film={props.result} />
+    }
+};
+
+export default Result;
